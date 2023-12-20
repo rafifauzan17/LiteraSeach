3ac176ce982886d416f713b8ab57ad086f416222
rafifauzan17 <71580377+rafifauzan17@users.noreply.github.com>
2023-12-20 20:06:58 +0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,20 +3,16 @@ const authRoutes = require("./routes/authRoutes");
 const bookRoutes = require("./routes/booksRoutes");
 const app = express();
 
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json")
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.use("/api/books", bookRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`LiteraSearch app listening on port ${PORT}`);
 });
--- src/controllers/bookControllers.js
+++ src/controllers/bookControllers.js
@@ -3,11 +3,30 @@ const pool = require("../config/database");
 const { response } = require("express");
 
 async function getBooks(req, res) {
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.size) || 20;
   try {
+    const start = (page - 1) * size;
     const [rows] = await pool.query(
-      "SELECT title, author, isbn, publication_year, publisher FROM Book LIMIT 1000"
+      "SELECT title, author, isbn, publication_year, publisher, id_perpus FROM buku LIMIT ?, ?",
+      [start, size]
     );
-    return res.status(200).json({ status: "success", data: rows });
+
+    const coverUrlsPromises = rows.map(async (row) => {
+      const isbn = row.isbn;
+      const coverUrl = `http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`;
+      return coverUrl;
+    });
+
+    const coverUrls = await Promise.all(coverUrlsPromises);
+
+    // Add coverUrls to the response
+    const booksWithCovers = rows.map((book, index) => ({
+      ...book,
+      coverUrl: coverUrls[index],
+    }));
+
+    return res.status(200).json({ status: "success", data: booksWithCovers });
   } catch (error) {
     return res.status(500).json({ status: "fail", data: error });
   }
@@ -15,6 +34,8 @@ async function getBooks(req, res) {
 
 async function getBook(req, res) {
   const searchTerm = req.params.any;
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.size) || 20;
 
   if (searchTerm !== null) {
     try {
@@ -27,7 +48,9 @@ async function getBook(req, res) {
       const whereClause =
         conditions.length > 0 ? `WHERE ${conditions.join(" OR ")}` : "";
 
-      const query = `SELECT title, author, isbn, publication_year, publisher, 
+      const start = (page - 1) * size;
+
+      const query = `SELECT title, author, isbn, publication_year, publisher, id_perpus,
                       CASE
                         WHEN title COLLATE utf8mb4_general_ci LIKE '%${searchTerm}%' THEN 'title'
                         WHEN author COLLATE utf8mb4_general_ci LIKE '%${searchTerm}%' THEN 'author'
@@ -35,10 +58,25 @@ async function getBook(req, res) {
                         WHEN isbn LIKE '%${searchTerm}%' THEN 'isbn'
                         ELSE NULL
                       END AS matched_column
-                    FROM Book ${whereClause}`;
+                    FROM buku ${whereClause} LIMIT ?, ?`;
 
-      const [rows] = await pool.query(query);
-      return res.status(200).json({ status: "success", data: rows });
+      const [rows] = await pool.query(query, [start, size]);
+
+      const coverUrlsPromises = rows.map(async (row) => {
+        const isbn = row.isbn;
+        const coverUrl = `http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`;
+        return coverUrl;
+      });
+
+      const coverUrls = await Promise.all(coverUrlsPromises);
+
+      // Add coverUrls to the response
+      const booksWithCovers = rows.map((book, index) => ({
+        ...book,
+        coverUrl: coverUrls[index],
+      }));
+
+      return res.status(200).json({ status: "success", data: booksWithCovers });
     } catch (error) {
       return res.status(500).json({ status: "fail", data: error });
     }
@@ -49,49 +87,41 @@ async function getBook(req, res) {
   }
 }
 
-async function getPopularBooks(req, res) {
+async function getTopBooks(req, res) {
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.size) || 20;
   try {
+    const topBooksUrl = "http://127.0.0.1:5000/books";
+    const topBooksResponse = await fetch(topBooksUrl);
+    const topBooksData = await topBooksResponse.json();
+
+    const bookTitles = topBooksData.book_name;
+
+    const titlePlaceholders = Array(bookTitles.length).fill("?").join(", ");
+
+    const start = (page - 1) * size;
     const [rows] = await pool.query(
-      "SELECT title, author, isbn, publication_year, publisher FROM Book ORDER BY average_rating DESC LIMIT 15"
+      `SELECT title, author, isbn, publication_year, publisher, id_perpus FROM buku WHERE title COLLATE utf8mb4_general_ci IN (${titlePlaceholders}) LIMIT ?, ?`,
+      [...bookTitles, start, size]
     );
-    return res.status(200).json({ status: "success", data: rows });
-  } catch (error) {
-    return res.status(500).json({ status: "fail", data: error });
-  }
-}
 
-async function getBookCovers(req, res) {
-  const isbn = req.params.isbn;
-  try {
-    const [rows] = await pool.query("SELECT isbn FROM Book WHERE isbn LIKE ?", [
-      [`%${isbn}%`],
-    ]);
-
-    if (rows.length === 0) {
-      return res
-        .status(404)
-        .json({ status: "fail", message: "ISBN tidak ditemukan" });
-    }
-
-    const keysUrls = rows.map((row) => {
+    const coverUrlsPromises = rows.map(async (row) => {
       const isbn = row.isbn;
-      return `http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`;
+      const coverUrl = `http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`;
+      return coverUrl;
     });
 
-    const response = await fetch(keysUrls);
+    const coverUrls = await Promise.all(coverUrlsPromises);
 
-    if (!response.headers.get("content-type").includes("application/json")) {
-      res.setHeader("Content-Type", "image/jpeg");
-      response.body.pipe(res);
-    } else {
-      res
-        .status(404)
-        .json({ status: "fail", message: "Gambar tidak ditemukan" });
-    }
+    // Add coverUrls to the response
+    const booksWithCovers = rows.map((book, index) => ({
+      ...book,
+      coverUrl: coverUrls[index],
+    }));
+
+    return res.status(200).json({ status: "success", data: booksWithCovers });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ status: "fail", message: "Buku tidak ditemukan" });
+    return res.status(500).json({ status: "fail", data: error });
   }
 }
 
@@ -99,15 +129,20 @@ async function getBookInfo(req, res) {
   const isbn = req.params.isbn;
 
   try {
-    const [rows] = await pool.query("SELECT isbn FROM Book WHERE isbn LIKE ?", [
-      [`%${isbn}%`],
-    ]);
+    const [rows] = await pool.query(
+      "SELECT isbn, id_perpus FROM buku WHERE isbn LIKE ?",
+      [[`%${isbn}%`]]
+    );
 
     if (rows.length === 0) {
       return res
         .status(404)
         .json({ status: "fail", message: "ISBN tidak ditemukan" });
     }
+
+    const idPerpus = rows.map((row) => {
+      return row.id_perpus;
+    });
 
     const keysUrls = rows.map((row) => {
       const isbn = row.isbn;
@@ -118,7 +153,7 @@ async function getBookInfo(req, res) {
     const keyResponses = await Promise.all(
       keysUrls.map(async (url) => {
         const response = await fetch(url);
-        return response.json(); // Assuming the response is in JSON format
+        return response.json();
       })
     );
 
@@ -137,6 +172,7 @@ async function getBookInfo(req, res) {
         {
           subjects: response?.subjects || null,
           description: response?.description || null,
+          id_perpus: idPerpus || null,
         },
       ];
     });
@@ -150,8 +186,10 @@ async function getBookInfo(req, res) {
   }
 }
 
-async function getBookSubject(req, res) {
+async function getBooksSubject(req, res) {
   const subject = req.params.subject;
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.size) || 20;
 
   try {
     const subjectApiUrl = `http://openlibrary.org/subjects/${subject}.json`;
@@ -166,13 +204,12 @@ async function getBookSubject(req, res) {
     }
 
     const titleWithSubject = subjectApiData.works.map((work) => work.title);
+    const start = (page - 1) * size;
 
     const placeholders = titleWithSubject.map(() => "?").join(", ");
-    console.log(placeholders);
-
     const [rows] = await pool.query(
-      `SELECT DISTINCT title, author, isbn, publication_year, publisher FROM Book WHERE title COLLATE utf8mb4_general_ci IN (${placeholders}) `,
-      titleWithSubject
+      `SELECT DISTINCT title, author, isbn, publication_year, publisher, id_perpus FROM buku WHERE title COLLATE utf8mb4_general_ci IN (${placeholders}) LIMIT ?, ?`,
+      [...titleWithSubject, start, size]
     );
 
     if (rows.length > 0) {
@@ -193,11 +230,98 @@ async function getBookSubject(req, res) {
   }
 }
 
+async function getTrendingBooks(req, res) {
+  const trending = req.params.any || "daily";
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.size) || 20;
+
+  try {
+    const booksUrl = `https://openlibrary.org/trending/${trending}.json`;
+    const booksUrlResponse = await fetch(booksUrl);
+    const booksUrlData = await booksUrlResponse.json();
+
+    const booksTitles = booksUrlData.works.map((work) => work.title);
+
+    const titlePlaceholders = Array(booksTitles.length).fill("?").join(", ");
+
+    const start = (page - 1) * size;
+    const [rows] = await pool.query(
+      `SELECT title, author, isbn, publication_year, publisher, id_perpus FROM buku WHERE title COLLATE utf8mb4_general_ci IN (${titlePlaceholders}) LIMIT ?, ?`,
+      [...booksTitles, start, size]
+    );
+
+    const coverUrlsPromises = rows.map(async (row) => {
+      const isbn = row.isbn;
+      const coverUrl = `http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`;
+      return coverUrl;
+    });
+
+    const coverUrls = await Promise.all(coverUrlsPromises);
+
+    // Add coverUrls to the response
+    const booksWithCovers = rows.map((book, index) => ({
+      ...book,
+      coverUrl: coverUrls[index],
+    }));
+
+    return res.status(200).json({ status: "success", data: booksWithCovers });
+  } catch (error) {
+    return res.status(500).json({ status: "fail", data: error });
+  }
+}
+
+async function getLocation(req, res) {
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+
+  try {
+    const locationRecommendation = await fetch(
+      `http://127.0.0.1:5000/library_recommendation?latitude=${latitude}&longitude=${longitude}`
+    );
+    const locationData = await locationRecommendation.json();
+
+    return res.status(200).json({ status: "success", data: locationData });
+  } catch (error) {
+    return res.status(500).json({ status: "fail", data: error });
+  }
+}
+
+async function getLibraries(req, res) {
+  try {
+    const [rows] = await pool.query("SELECT * FROM perpustakaan");
+
+    return res.status(200).json({ status: "success", data: rows });
+  } catch (error) {
+    return res.status(500).json({ status: "fail", data: error });
+  }
+}
+
+async function getLibraryBooks(req, res) {
+  const id_perpus = req.params.id_perpus;
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.size) || 20;
+
+  try {
+    const start = (page - 1) * size;
+    const [rows] = await pool.query(
+      "SELECT title, author, isbn, publication_year, publisher, id_perpus FROM buku WHERE id_perpus LIKE ? LIMIT ?, ?",
+      [`%${id_perpus}%`, start, size]
+    );
+
+    return res.status(200).json({ status: "success", data: rows });
+  } catch (error) {
+    return res.status(500).json({ status: "fail", data: error });
+  }
+}
+
 module.exports = {
   getBooks,
   getBook,
-  getPopularBooks,
-  getBookCovers,
+  getTopBooks,
   getBookInfo,
-  getBookSubject,
+  getBooksSubject,
+  getTrendingBooks,
+  getLocation,
+  getLibraries,
+  getLibraryBooks,
 };
