9e9047df5480a8d20f2f523759be7b086f6b5f6a
rafifauzan17 <71580377+rafifauzan17@users.noreply.github.com>
2023-12-20 20:35:05 +0700

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authControllers");
 
-router.post("/signin", authController.signInWithCustomToken);
-
-module.exports = router;
